b2573df3f41ffc9b31d4d8713f5671e0f2e79573
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-02-15 08:31:49 +0200

    Add start script

--- start.sh
+++ start.sh
@@ -2,9 +2,13 @@
 # This script should run as root since the servers use docker.
 
 FRONTEND_PORT=$1
+if [ -z "$FRONTEND_PORT" ]; then
+   FRONTEND_PORT=80
+fi
 
 cd backend
 nohup python3 server.py &
 
 cd ../frontend
 nohup python3 -m http.server FRONTEND_PORT &
+echo "Try me by: lynx http://localhost:80"
