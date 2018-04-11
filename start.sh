ea789c5b3205152c84a0a304162f25fc69253828
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-04-11 21:37:24 +0300

    minor update

--- start.sh
+++ start.sh
@@ -15,10 +15,12 @@ sudo docker run --name badkan --rm -itd erelsgl/badkan bash
 # 2. Start the backend server:
 cd backend
 sudo rm nohup.out
-sudo nohup python3 server.py &
+sudo nohup python3 -u server.py &
+# -u = unbuffered. See https://stackoverflow.com/a/107717/827927
 
 # 2. Start the frontend server:
 cd ../frontend
 sudo rm nohup.out
-sudo nohup python3 -m http.server $FRONTEND_PORT &
+sudo nohup python3 -u -m http.server $FRONTEND_PORT &
+# -u = unbuffered. See https://stackoverflow.com/a/107717/827927
 echo "Try me by: lynx http://localhost:$FRONTEND_PORT"
