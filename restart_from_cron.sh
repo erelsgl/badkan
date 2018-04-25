95c2746ed7cb0864bfbf770411f2e06ee258dc97
root <root@server.arielun>
2018-04-25 21:51:34 +0300

    make 10 servers

--- restart_from_cron.sh
+++ restart_from_cron.sh
@@ -8,7 +8,16 @@ sudo killall python3
 # 1. Start the backend server:
 cd backend
 sudo rm nohup.out
-sudo python3 -u server.py > nohup.out &
+sudo nohup python3 -u server.py 5670 &
+sudo nohup python3 -u server.py 5671 &
+sudo nohup python3 -u server.py 5672 &
+sudo nohup python3 -u server.py 5673 &
+sudo nohup python3 -u server.py 5674 &
+sudo nohup python3 -u server.py 5675 &
+sudo nohup python3 -u server.py 5676 &
+sudo nohup python3 -u server.py 5677 &
+sudo nohup python3 -u server.py 5678 &
+sudo nohup python3 -u server.py 5679 &
 
 # 2. Start the frontend server:
 cd ../frontend
--- start.sh
+++ start.sh
@@ -2,13 +2,10 @@
 
 FRONTEND_PORT=$1
 FRONTEND_Test_PORT=$2
+
 if [ -z "$FRONTEND_PORT" ]; then
    FRONTEND_PORT=80
 fi
-
-# NOTE: the backend port is set in backend/server.py.
-# It is identical to the port in frontend/index.html.
-# Initially it was 5678.
 
 # 1. Start the docker process:
 sudo docker run --name badkan --rm -itd erelsgl/badkan bash
@@ -16,7 +13,16 @@ sudo docker run --name badkan --rm -itd erelsgl/badkan bash
 # 2. Start the backend server:
 cd backend
 sudo rm nohup.out
-sudo nohup python3 -u server.py &
+sudo nohup python3 -u server.py 5670 &
+sudo nohup python3 -u server.py 5671 &
+sudo nohup python3 -u server.py 5672 &
+sudo nohup python3 -u server.py 5673 &
+sudo nohup python3 -u server.py 5674 &
+sudo nohup python3 -u server.py 5675 &
+sudo nohup python3 -u server.py 5676 &
+sudo nohup python3 -u server.py 5677 &
+sudo nohup python3 -u server.py 5678 &
+sudo nohup python3 -u server.py 5679 &
 # -u = unbuffered. See https://stackoverflow.com/a/107717/827927
 
 # 2. Start the frontend server:
