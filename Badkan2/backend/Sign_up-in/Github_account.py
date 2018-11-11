785f85e5f37bdb30a2789eab0fc06473456057fa
Samuel <sam101@hotmail.fr>
2018-11-11 10:09:51 +0200

    two new problem that we need to clear

--- Badkan2/backend/Sign_up-in/Github_account.py
+++ Badkan2/backend/Sign_up-in/Github_account.py
@@ -29,3 +29,24 @@ authorization_response=redirect_response)
 # Fetch a protected resource, i.e. user profile
 r = github.get('https://api.github.com/user')
 print(r.content)
+
+# The content printed is as the next:
+# b'{"login":"SamuelBismuth","id":33607401,"node_id":"MDQ6VXNlcjMzNjA3NDAx",
+# "avatar_url":"https://avatars0.githubusercontent.com/u/33607401?v=4","
+# gravatar_id":"","url":"https://api.github.com/users/SamuelBismuth",
+# "html_url":"https://github.com/SamuelBismuth","followers_url":"https://api.github.com/users/SamuelBismuth/followers",
+# "following_url":"https://api.github.com/users/SamuelBismuth/following{/other_user}",
+# "gists_url":"https://api.github.com/users/SamuelBismuth/gists{/gist_id}",
+# "starred_url":"https://api.github.com/users/SamuelBismuth/starred{/owner}{/repo}",
+# "subscriptions_url":"https://api.github.com/users/SamuelBismuth/subscriptions",
+# "organizations_url":"https://api.github.com/users/SamuelBismuth/orgs",
+# "repos_url":"https://api.github.com/users/SamuelBismuth/repos","events_url":"https://api.github.com/users/SamuelBismuth/events{/privacy}","
+# received_events_url":"https://api.github.com/users/SamuelBismuth/received_events","type":"User","site_admin":false,
+# "name":"Samuel Bismuth","company":null,"blog":"","location":"Israel","email":null,"hireable":null,"bio":null,"public_repos":17,
+# "public_gists":0,"followers":4,"following":7,"created_at":"2017-11-12T20:51:22Z","updated_at":"2018-10-18T15:57:59Z"}'
+
+# The question is how should we handle the data, and how it's work about the password? Another thing is how should we do
+# if the login is already in use? Maybe the solution is to attribute to the new user a new user name, and allows him to
+# connect only via github, or give him a new password as weel for the normal connection?
+
+# Second issue: is this work should be done in the backend or the frontend??
