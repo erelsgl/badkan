8901914f37a691fd844f7ea350f24a414411d677
root <sam101@hotmail.fr>
2019-03-10 15:45:08 +0000

    firebase config

--- frontend/util/FirebaseConfig.js
+++ frontend/util/FirebaseConfig.js
@@ -1,10 +1,10 @@
-alert("Insert your FIrebase details in frontend/util/FirebaseConfig.js")
 var config = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "..."
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyCLsZO2x5fZQP1woZV0z3WUWaXI_VvMd0M",
+    authDomain: "badkan-9d48d.firebaseapp.com",
+    databaseURL: "https://badkan-9d48d.firebaseio.com",
+    projectId: "badkan-9d48d",
+    storageBucket: "badkan-9d48d.appspot.com",
+    messagingSenderId: "253448259334"
+  };
+  firebase.initializeApp(config);
+
