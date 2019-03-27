99c55df9236e36f66e2ef98bf981a44f07fcb63c
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-03-27 18:19:52 +0200

    fix backup script

--- start/export_exercises.sh
+++ start/export_exercises.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # Database JSON export (for backup)
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/..
 tar -zcvf database_exports/exercises-backup.tar.gz exercises
-cd database_exports;   python3 ../start/firebase_save.py
+
+cd $DIR/../database_exports
+python3 ../start/firebase_save.py
