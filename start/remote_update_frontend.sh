3da76c4dc7dd942b05a4331724d485468ff3ad49
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-05-11 21:58:59 +0300

    remote

--- start/remote_update_frontend.sh
+++ start/remote_update_frontend.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# CURRENTLY DOES NOT WORK - PROBLEM WITH  nohup
 #
 # Run this script from your home computer in order to automatically update the frontend from github
 #
 
 git push
-ssh root@104.248.40.179 "cd /root/badkan; source start/update_frontend.sh"
+ssh root@104.248.40.179 "cd /root/badkan; bash start/update_frontend.sh"
