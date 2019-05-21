51e96b2e64da692effc3f16d06986b69ae2f329f
cyberlab <sam101@hotmail.fr>
2019-05-21 16:31:31 +0300

    test commit

--- frontend/app/Conflicts.js
+++ frontend/app/Conflicts.js
@@ -5,4 +5,15 @@ let uid = JSON.parse(localStorage.getItem("homeUserId"));
 let exerciseId = getParameterByName("exercise"); // in utils.js
 
 // Then, retreive the data from firebase.
-getConflictsByUid(exerciseId, uid);
+getConflictsByUid(exerciseId, uid, addItemToList);
+
+
+function addItemToList(functionName, numOfReclamation) {
+    // TODO: add the test itself then remove conflicts object and rename tests to conflict.
+    let txtHtml = "<pre>";
+    txtHtml += "Name of the test function: " + functionName + ". <br />";
+    txtHtml += "Number of reclamation(s): " + numOfReclamation + ".  <br />";
+    txtHtml += "<input type=\"checkbox\" name=\"wrong\" id=\"" + functionName + "\" value=\"Wrong\">Wrong reclamation"
+    txtHtml += "</pre>";
+    $("div#list").append(txtHtml)
+}
