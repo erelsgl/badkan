c99a605f44b8e156adad0d07344b602f1391abe7
cyberlab <sam101@hotmail.fr>
2019-05-22 16:01:55 +0300

    fix

--- frontend/util/Notif.js
+++ frontend/util/Notif.js
@@ -28,60 +28,59 @@ $(document).ready(function () {
         localStorage.setItem("homeUserKey", JSON.stringify(notifHomeUser));
         writeUserDataWithoutComingHome(notifHomeUser, notifUid)
     }
-}
     if (numNotif === 0) {
-    $('#noti_Counter').hide();
-} else {
-    $('#noti_Counter').show();
-}
-// ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
-$('#noti_Counter')
-    .css({
-        opacity: 0
-    })
-    .text(numNotif) // ADD DYNAMIC VALUE (YOU CAN EXTRACT DATA FROM DATABASE OR
-    // XML).
-    .css({
-        top: '-10px'
-    })
-    .animate({
-        top: '-2px',
-        opacity: 1
-    }, 500);
+        $('#noti_Counter').hide();
+    } else {
+        $('#noti_Counter').show();
+    }
+    // ANIMATEDLY DISPLAY THE NOTIFICATION COUNTER.
+    $('#noti_Counter')
+        .css({
+            opacity: 0
+        })
+        .text(numNotif) // ADD DYNAMIC VALUE (YOU CAN EXTRACT DATA FROM DATABASE OR
+        // XML).
+        .css({
+            top: '-10px'
+        })
+        .animate({
+            top: '-2px',
+            opacity: 1
+        }, 500);
 
-// Append here the notification to show to the user.
+    // Append here the notification to show to the user.
 
 
-$('#noti_Button').click(function () {
-    // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
-    $('#notifications').fadeToggle('fast', 'linear', function () {
-        if ($('#notifications').is(':hidden')) {
-            $('#noti_Button').css('background-color', '#2E467C');
-        }
-        // CHANGE BACKGROUND COLOR OF THE BUTTON.
-        else
-            $('#noti_Button').css('background-color', '#FFF');
+    $('#noti_Button').click(function () {
+        // TOGGLE (SHOW OR HIDE) NOTIFICATION WINDOW.
+        $('#notifications').fadeToggle('fast', 'linear', function () {
+            if ($('#notifications').is(':hidden')) {
+                $('#noti_Button').css('background-color', '#2E467C');
+            }
+            // CHANGE BACKGROUND COLOR OF THE BUTTON.
+            else
+                $('#noti_Button').css('background-color', '#FFF');
+        });
+
+        $('#noti_Counter').fadeOut('slow'); // HIDE THE COUNTER.
+
+        return false;
     });
 
-    $('#noti_Counter').fadeOut('slow'); // HIDE THE COUNTER.
+    // HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
+    $(document).click(function () {
+        $('#notifications').hide();
 
-    return false;
-});
+        // CHECK IF NOTIFICATION COUNTER IS HIDDEN.
+        if ($('#noti_Counter').is(':hidden')) {
+            // CHANGE BACKGROUND COLOR OF THE BUTTON.
+            $('#noti_Button').css('background-color', '#2E467C');
+        }
+    });
 
-// HIDE NOTIFICATIONS WHEN CLICKED ANYWHERE ON THE PAGE.
-$(document).click(function () {
-    $('#notifications').hide();
-
-    // CHECK IF NOTIFICATION COUNTER IS HIDDEN.
-    if ($('#noti_Counter').is(':hidden')) {
-        // CHANGE BACKGROUND COLOR OF THE BUTTON.
-        $('#noti_Button').css('background-color', '#2E467C');
-    }
-});
-
-$('#notifications').click(function () {
-    return false; // DO NOTHING WHEN CONTAINER IS CLICKED.
-});
+    $('#notifications').click(function () {
+        return false; // DO NOTHING WHEN CONTAINER IS CLICKED.
+    });
 });
 
 function addNonReadedNotif(message, index) {
