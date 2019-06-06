ef47c81872fe0983dc1ed8789d70ec70d52d2970
Erel Segal-Halevi cron <erelsgl@gmail.com>
2019-06-06 21:52:45 +0300

    add timestamp; remove trace_table

--- backend/util/update_courses.py
+++ backend/util/update_courses.py
@@ -23,12 +23,16 @@ def update_courses():
     id = re.search('projectId: "(.+?)",', data)
     FIREBASE_APP = id.group(1)
 
-    r = requests.get('https://'+FIREBASE_APP +
-                     '.firebaseio.com/courses.json?format=export')
+    url = 'https://'+FIREBASE_APP +'.firebaseio.com/courses.json?format=export'
+    r = requests.get(url)
+    if r.status_code!=200:
+        raise(RuntimeError("Error in reading {}: code is {}".format(url,r.status_code)))
     coursesObject = r.json()
 
-    r = requests.get('https://'+FIREBASE_APP +
-                     '.firebaseio.com/exercises.json?format=export')
+    url = 'https://'+FIREBASE_APP +'.firebaseio.com/exercises.json?format=export'
+    r = requests.get(url)
+    if r.status_code!=200:
+        raise(RuntimeError("Error in reading {}: code is {}".format(url,r.status_code)))
     exercisesObject = r.json()
 
     if coursesObject:
