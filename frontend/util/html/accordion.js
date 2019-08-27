c52ea3099f7f572573c0855e22372d9703bd7f46
Samuel <sam101@hotmail.fr>
2019-08-27 18:31:23 +0200

    push for compare

--- frontend/util/html/accordion.js
+++ frontend/util/html/accordion.js
@@ -56,10 +56,10 @@ function createAccordionBodyManageCourse(courseObj) {
 	let course = courseObj[1]
 	let html = '<div class="panel" style="background:transparent">' +
 		'<div class="course">' +
-		'<label for="courseInputEdit"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
-		'<input id="course_name' + courseId + '" class="courseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>' +
-		'<label for="courseInputEdit"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
-		'<input id="course_grader' + courseId + '" class="courseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>' +
+		'<label for="course_name"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
+		'<input id="course_name' + courseId + '" class="courseExerciseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>' +
+		'<label for="course_grader"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
+		'<input id="course_grader' + courseId + '" class="courseExerciseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>' +
 		'<label for="privacyEdit"><div class="explanation" data-toggle="tooltip" title="The course is shared only with the students you want.">Privacy</div></label><br>' +
 		'<input type="radio" name="privacy' + courseId + '" value="public" onclick=\'$(\"#pass' + courseId + '\").hide()\'' +
 		((course.privacy == 'public') ? "checked" : "") +
@@ -69,8 +69,8 @@ function createAccordionBodyManageCourse(courseObj) {
 		'> Private</input><br><br>' +
 		'<div id="pass' + courseId + '"' +
 		((course.privacy == 'public') ? "style=display:none;>" : ">") +
-		'<label for="ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
-		'<input id="course_ids' + courseId + '" class="ids" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
+		'<label for="course_ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
+		'<input id="course_ids' + courseId + '" class="ids courseExerciseInputEdit" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
 		'</div>' + '<br>' +
 		'<button class="btn btn_edit" onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
 		'<button class="btn btn_edit" onclick="deleteCourse(' + "'" + courseId + "'" + ')">Delete course <i class="glyphicon glyphicon-trash"></i></button>' +
@@ -82,24 +82,24 @@ function createAccordionBodyManageExercise(exerciseObj) {
 	let exerciseId = exerciseObj[0]
 	let exercise = exerciseObj[1]
 	let html = '<div class="panel" style="background:transparent">' +
-		'<div class="course">' +
-		// 	'<label for="courseInputEdit"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
-		// 	'<input id="course_name' + courseId + '" class="courseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>'+
-		// 	'<label for="courseInputEdit"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
-		// 	'<input id="course_grader' + courseId + '" class="courseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>'+
-		// 	'<label for="privacyEdit"><div class="explanation" data-toggle="tooltip" title="The course is shared only with the students you want.">Privacy</div></label><br>' +
-		// 	'<input type="radio" name="privacy' + courseId + '" value="public" onclick=\'$(\"#pass' + courseId + '\").hide()\'' +
-		// 	((course.privacy == 'public') ? "checked" : "") +
-		// 	'> Public</input><br>' +
-		// 	'<input type="radio" name="privacy' + courseId + '" value="private" onclick=\'$(\"#pass' + courseId + '\").show()\'' +
-		// 	((course.privacy == 'private') ? "checked" : "") +
-		// 	'> Private</input><br><br>' +
-		// 	'<div id="pass' + courseId + '"' +
-		// 	((course.privacy == 'public') ? "style=display:none;>" : ">") +
-		// 	'<label for="ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
-		// 	'<input id="course_ids' + courseId + '" class="ids" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
-		// 	'</div>' + '<br>'+
-		// 	'<button class="btn btn_edit" onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
+		'<div class="exercise">' +
+		// '<label for="course_name"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
+		// '<input id="course_name' + courseId + '" class="courseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>' +
+		// '<label for="course_grader"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
+		// '<input id="course_grader' + courseId + '" class="courseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>' +
+		// '<label for="privacyEdit"><div class="explanation" data-toggle="tooltip" title="The course is shared only with the students you want.">Privacy</div></label><br>' +
+		// '<input type="radio" name="privacy' + courseId + '" value="public" onclick=\'$(\"#pass' + courseId + '\").hide()\'' +
+		// ((course.privacy == 'public') ? "checked" : "") +
+		// '> Public</input><br>' +
+		// '<input type="radio" name="privacy' + courseId + '" value="private" onclick=\'$(\"#pass' + courseId + '\").show()\'' +
+		// ((course.privacy == 'private') ? "checked" : "") +
+		// '> Private</input><br><br>' +
+		// '<div id="pass' + courseId + '"' +
+		// ((course.privacy == 'public') ? "style=display:none;>" : ">") +
+		// '<label for="course_ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
+		// '<input id="course_ids' + courseId + '" class="ids" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
+		// '</div>' + '<br>' +
+		// '<button class="btn btn_edit" onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
 		// '<button class="btn btn_edit" onclick="deleteCourse(' + "'" + courseId + "'" + ')">Delete course <i class="glyphicon glyphicon-trash"></i></button>' +
 		'</div></div>';
 	return html;
