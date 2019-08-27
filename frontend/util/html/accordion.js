711dc009d1b3c0c7e5cd46954886d1576b188652
Jeremy <lznjeremy@gmail.com>
2019-08-27 12:34:36 +0300

    finish register course and manage exercice Signed-off-by: Jeremy <lznjeremy@gmail.com>

--- frontend/util/html/accordion.js
+++ frontend/util/html/accordion.js
@@ -7,8 +7,8 @@ function createAccordionManage(courseObj, exercises) {
 	// for (exercise of exercises) {
 	// 	panel += createAccordionBodyManage(course, exercise)
 	// }
-	panel += '<div class="panel"><button id=newExercise data-toggle="tooltip" title="New exercise" ' +
-		'class="plus-button addExercise" onclick="newExercise(' + "'" + courseId + "'" +')"></button></div>'
+	panel += '<button id=newExercise data-toggle="tooltip" title="New exercise" ' +
+		'class="plus-button addExercise" onclick="newExercise(' + "'" + courseId + "'" +')"></button>'
 	$(".nacc").append(panel + "</li>")
 }
 
@@ -32,15 +32,18 @@ function createAccordionBodyHomeRegister(exercise) {
 	let grade = 0;
 	let description = "Short description:";
 	let timeStamp = "Thu Jun 06 2019 12:21:34 GMT+0300 (Israel Daylight Time)."
-	return '<div class="panel" style="background: rgb(211, 230, 241)";>' +
+	return '<div class="panel" style="background: transparent">' +
 		'<div class="exercise" name="Vu1XBFXwv7aXLWnWuTADwBUOzQD2_1">' +
 		'<div class="exerciseName">' + exerciseName + '</div>' +
-		'<div class="description">Short description: ' + description + '</div>' +
+		'<br>'+
+		'<div class="description" style="font-family:monospace">Short description: ' + description + '</div>' +
 		'<div class="btn btn-link">Download PDF</div>' +
 		'<div class="result">' +
-		'<div class=countryId>For the submission with the id(s): ' + userDetails.country_id + '</div>' +
-		'<div class=grade>Your current grade is: ' + grade + '</div>' +
-		'<div class=timestamp>Submitted on:' + timeStamp + '</div>' +
+		'<div class=countryId style="font-family:monospace">For the submission with the id(s): ' + userDetails.country_id + '</div>' +
+		'<div class=grade style="font-family:monospace">Your current grade is: ' + grade + '</div>' +
+		'<br>'+
+		'<div class=timestamp style="font-family:URW Chancery L, cursive">Submitted on:' + timeStamp + '</div>' +
+		'<br>'+
 		'<div class="btn btn-success">Solve</button>' +
 		'</div>' +
 		'</div>' +
@@ -51,12 +54,12 @@ function createAccordionBodyHomeRegister(exercise) {
 function createAccordionBodyManageExercise(courseObj) {
 	let courseId = courseObj[0]
 	let course = courseObj[1]
-	let html = '<div class="panel">' +
+	let html = '<div class="panel" style="background:transparent">' +
 		'<div class="course">' +
 		'<label for="courseInputEdit"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
-		'<input id="course_name' + courseId + '" class="courseInputEdit" value="' + course.course_name + '"></input><br>' +
+		'<input id="course_name' + courseId + '" class="courseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>'+
 		'<label for="courseInputEdit"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
-		'<input id="course_grader' + courseId + '" class="courseInputEdit" value="' + course.grader_uid + '"></input><br>' +
+		'<input id="course_grader' + courseId + '" class="courseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>'+
 		'<label for="privacyEdit"><div class="explanation" data-toggle="tooltip" title="The course is shared only with the students you want.">Privacy</div></label><br>' +
 		'<input type="radio" name="privacy' + courseId + '" value="public" onclick=\'$(\"#pass' + courseId + '\").hide()\'' +
 		((course.privacy == 'public') ? "checked" : "") +
@@ -67,9 +70,9 @@ function createAccordionBodyManageExercise(courseObj) {
 		'<div id="pass' + courseId + '"' +
 		((course.privacy == 'public') ? "style=display:none;>" : ">") +
 		'<label for="ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
-		'<input id="course_ids' + courseId + '" class="ids" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000""></input><br>' +
-		'</div>' +
-		'<button onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
+		'<input id="course_ids' + courseId + '" class="ids" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
+		'</div>' + '<br>'+
+		'<button class="btn btn_edit" onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
 		'</div></div>';
 	return html;
 }
