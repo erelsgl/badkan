72461b91c25413885b519df96e738224be6f1605
Samuel <sam101@hotmail.fr>
2019-08-27 19:24:05 +0200

    almost done with the edit exercise :)

--- frontend/util/html/accordion.js
+++ frontend/util/html/accordion.js
@@ -56,9 +56,9 @@ function createAccordionBodyManageCourse(courseObj) {
 	let course = courseObj[1]
 	let html = '<div class="panel" style="background:transparent">' +
 		'<div class="course">' +
-		'<label for="course_name"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
+		'<label for="course_name' + courseId + '"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
 		'<input id="course_name' + courseId + '" class="courseExerciseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>' +
-		'<label for="course_grader"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
+		'<label for="course_grader' + courseId + '"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
 		'<input id="course_grader' + courseId + '" class="courseExerciseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>' +
 		'<label for="privacyEdit"><div class="explanation" data-toggle="tooltip" title="The course is shared only with the students you want.">Privacy</div></label><br>' +
 		'<input type="radio" name="privacy' + courseId + '" value="public" onclick=\'$(\"#pass' + courseId + '\").hide()\'' +
@@ -69,7 +69,7 @@ function createAccordionBodyManageCourse(courseObj) {
 		'> Private</input><br><br>' +
 		'<div id="pass' + courseId + '"' +
 		((course.privacy == 'public') ? "style=display:none;>" : ">") +
-		'<label for="course_ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
+		'<label for="course_ids' + courseId + '"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
 		'<input id="course_ids' + courseId + '" class="ids courseExerciseInputEdit" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
 		'</div>' + '<br>' +
 		'<button class="btn btn_edit" onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
@@ -83,10 +83,32 @@ function createAccordionBodyManageExercise(exerciseObj) {
 	let exercise = exerciseObj[1]
 	let html = '<div class="panel" style="background:transparent">' +
 		'<div class="exercise">' +
-		// '<label for="course_name"><div class="explanation" data-toggle="tooltip" title="Required field">Course name *</div></label>' +
-		// '<input id="course_name' + courseId + '" class="courseInputEdit" value="' + course.course_name + '"></input><br>' + '<br>' +
+		'<label for="exercise_name' + exerciseId + '"><div class="explanation" data-toggle="tooltip" title="Required field">Exercise name *</div></label>' +
+		'<input id="exercise_name' + exerciseId + '" class="courseExerciseInputEdit" value="' + exercise.exercise_name + '"></input><br>' + '<br>' +
+		'<label for="exercise_compiler' + exerciseId + '"><div class="explanation" data-toggle="tooltip" title="The compiler for the exercise.">Exercise compiler *</div></label>' +
+		'<select id="exercise_compiler' + exerciseId + '" class="swal2-input">' + /* For Jeremy you can play with the class swal2 if you want. */
+		'<option value="javac"' + (exercise.exercise_compiler == "javac" ? "selected" : "") + '>javac</option>' +
+		'<option value="gcc"' + (exercise.exercise_compiler == "gcc" ? "selected" : "") + '>gcc</option>' +
+		'<option value="clang"' + (exercise.exercise_compiler == "clang" ? "selected" : "") + '>clang</option>' +
+		'</select>' +
+
+		'<label for="submission_option' + exerciseId + '"><div class="explanation" data-toggle="tooltip" title="For each method checked, the student will be able to submit his exercise via the method.' +
+		'If you want the student only submit via GitHub, then check only the GitHub button .">Submission option *</div></label>' +
+		'<div id="submission_option' + exerciseId + '" >' +
+		'<input id="github' + exerciseId + '" name="BoxSelect[]" type="checkbox" value="github" required="" ' + 
+		(exercise.submission_via_github? "checked" : "") +
+		'>GitHub</input> <br>' +
+		'<input id="zip' + exerciseId + '" name="BoxSelect[]" type="checkbox" value="zip" required="" ' + 
+		(exercise.submission_via_zip? "checked" : "") +
+		'>Zip</input>' +
+		'</div>' +
+
+		'<label for="main_file' + exerciseId + '"><div class="explanation" data-toggle="tooltip" title="The file where the main function resides">Main file *</div></label>' +
+		'<input id="main_file' + exerciseId + '" class="courseExerciseInputEdit" value="' + exercise.main_file	 + '"></input><br>' + '<br>' +
+
+		
 		// '<label for="course_grader"><div class="explanation" data-toggle="tooltip" title="The grader must be admin. \nGives an access to the manage course.">Grader id \n </div></label>' +
-		// '<input id="course_grader' + courseId + '" class="courseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>' +
+		// '<input id="course_grader' + courseId + '" class="courseExerciseInputEdit" value="' + course.grader_uid + '" style="margin-left:88px"></input><br>' + '<br>' +
 		// '<label for="privacyEdit"><div class="explanation" data-toggle="tooltip" title="The course is shared only with the students you want.">Privacy</div></label><br>' +
 		// '<input type="radio" name="privacy' + courseId + '" value="public" onclick=\'$(\"#pass' + courseId + '\").hide()\'' +
 		// ((course.privacy == 'public') ? "checked" : "") +
@@ -97,7 +119,7 @@ function createAccordionBodyManageExercise(exerciseObj) {
 		// '<div id="pass' + courseId + '"' +
 		// ((course.privacy == 'public') ? "style=display:none;>" : ">") +
 		// '<label for="course_ids"><div class="explanation" data-toggle="tooltip" title="Please respect the format \nRequired field.">Students ids *</div></label>' +
-		// '<input id="course_ids' + courseId + '" class="ids" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
+		// '<input id="course_ids' + courseId + '" class="ids courseExerciseInputEdit" value="' + String(course.uids).replace(",", " ") + '" placeholder="000000000 000000000"" style="margin-left:47px"></input><br>' +
 		// '</div>' + '<br>' +
 		// '<button class="btn btn_edit" onclick="editCourse(' + "'" + courseId + "'" + ')">Edit course <i class="glyphicon glyphicon-edit"></i></button>' +
 		// '<button class="btn btn_edit" onclick="deleteCourse(' + "'" + courseId + "'" + ')">Delete course <i class="glyphicon glyphicon-trash"></i></button>' +
