314b5d4cc35020abce43373c729ff309f4f37cb7
Jeremy <lznjeremy@gmail.com>
2020-04-12 04:23:13 +0300

    update collab error message

--- backend/badkan/input_output/submission_handler.py
+++ backend/badkan/input_output/submission_handler.py
@@ -13,6 +13,8 @@ async def check_submission(websocket, submission):
                   submission["collab1"], submission["collab2"]]
     zip_filename = "../submissions/" + \
         submission["exercise_id"]+"/"+submission["uid"]+".zip"
+    if not await collaborator_not_exist(submission["collab1"], submission["collab2"], websocket) :
+        return
     if "github_url" in submission:
         matches = GIT_REGEXP.search(submission["github_url"])
         username = matches.group(1)
@@ -70,6 +72,33 @@ def save_zip_submission(zip_file, exercise_id, uid):
     zip_file.save("../submissions/" +
                   exercise_id + "/" + uid + ".zip")
 
+async def collaborator_not_exist(collab1, collab2, websocket):
+    if collab1=="" and collab2=="":
+        return True
+    elif collab2=="":
+        if get_uid_by_country_id(collab1) == None :
+            await tee(websocket, "The first collaborator id is not exist.")
+            await tee(websocket, "Please, check if your collaborator have a badkan account, and that his id match.")
+            return False
+    elif collab1=="":
+        if get_uid_by_country_id(collab2) == None :
+            await tee(websocket, "The second collaborator id is not exist.")
+            await tee(websocket, "Please, check if your collaborator have a badkan account, and that his id match.")
+            return False
+    else :
+        if get_uid_by_country_id(collab2) == None and get_uid_by_country_id(collab1) == None:
+            await tee(websocket, "The first and the second collaborators ids are not exist.")
+            await tee(websocket, "Please, check if yours collaborators have a badkan account, and that their ids match.")
+            return False
+        if get_uid_by_country_id(collab1) == None :
+            await tee(websocket, "The first collaborator id is not exist.")
+            await tee(websocket, "Please, check if your collaborator have a badkan account, and that his id match.")
+            return False
+        if get_uid_by_country_id(collab2) == None :
+            await tee(websocket, "The second collaborator id is not exist.")
+            await tee(websocket, "Please, check if your collaborator have a badkan account, and that his id match.")
+            return False
+    return True
 
 async def save_github_submission(submission, zip_filename, git_clone_url, uid, websocket):
     path = "../submissions/" + submission["exercise_id"]
