44a1fe701b29d4d168edb228ffd8906f22ba9ded
jeremylzn <lznjeremy@gmail.com>
2020-11-04 21:27:40 +0200

    fixed bug in gradin_room and grade

--- docker/files/run_zip_submission_with_zip_exercise.sh
+++ docker/files/run_zip_submission_with_zip_exercise.sh
@@ -5,28 +5,30 @@
 export FOLDER_NAME=$1
 export EXERCISE_ID=$2
 
-
-cd grading_room/$EXERCISE_ID
-unzip -qq ./$EXERCISE_ID.zip
-
-
-# make the name of the exercise to be "EXERCISE_ID"
-mv */ ./$EXERCISE_ID 2>/dev/null
-
 # unzip the submission.
-cd ../$FOLDER_NAME 
+cd grading_room/$FOLDER_NAME
 unzip -qq $FOLDER_NAME.zip
 
 # rename the submission folder to FOLDER_NAME
 mv */ ./$FOLDER_NAME 2>/dev/null
-# move all submission file to exercise folder
-mv -v $FOLDER_NAME/*  2>/dev/null ../$EXERCISE_ID/$EXERCISE_ID 
+cd ..
 
-cd ../$EXERCISE_ID/$EXERCISE_ID
+# make the name of the outside folder exercise to be UNIQUE "EXERCISE_ID+_FOLDER_NAME"
+mv ./$EXERCISE_ID ./$EXERCISE_ID$FOLDER_NAME
 
+cd ./$EXERCISE_ID$FOLDER_NAME
+unzip -qq ./$EXERCISE_ID.zip
+
+# make the name of the exercise to be "EXERCISE_ID_FOLDER_NAME"
+mv */ ./$EXERCISE_ID$FOLDER_NAME 2>/dev/null
+
+# move all submission file to submission folder
+mv -v ./$EXERCISE_ID$FOLDER_NAME/*  2>/dev/null ../$FOLDER_NAME/$FOLDER_NAME 
+
+cd ../$FOLDER_NAME/$FOLDER_NAME
 # process the grade.
 bash grade
 
 cd ../..
-rm -R ./$EXERCISE_ID
+rm -R ./$EXERCISE_ID$FOLDER_NAME
 rm -R $FOLDER_NAME
